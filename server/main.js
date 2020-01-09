fe10d10e2eb33bd48a30fea38d764b64e0d01903
strazan <sigge@labor.cool>
2020-01-09 21:25:30 +0100

    display json from APIs in html and in console

--- server/main.js
+++ server/main.js
@@ -1,14 +1,42 @@
-const express = require('express')
-const app = express()
-const port = 8080
-const fetch = require('node-fetch');
-var cors = require('cors')
-app.get('/', cors(), (req, res, next) => 
-    fetch('https://labs.thetrainbrain.com/knockon/?minutes=30')
-    .then (res => {
-        return res.json()
-    }).then(json => {
-        res.send(json)
+const express = require("express");
+const app = express();
+const port = 8080;
+const fetch = require("node-fetch");
+var cors = require("cors");
+
+app.get("/vehicle-movement", cors(), (req, res, next) =>
+  fetchAndSend(
+    "https://labs.thetrainbrain.com/realtimemap/public/positions.json",
+    res
+  )
+);
+
+app.get("/knock-on", cors(), (req, res, next) =>
+  fetchAndSend("https://labs.thetrainbrain.com/knockon/?minutes=30", res)
+);
+
+app.get("/storning", cors(), (req, res, next) =>
+  fetchAndSend(
+    "https://api.sl.se/api2/deviations.json?key=f5317e51b33d42bba32545f94341eeab",
+    res
+  )
+);
+
+app.get("/trafiklaget", cors(), (req, res, next) =>
+  fetchAndSend(
+    "https://api.sl.se/api2/trafficsituation.json?key=e1eb7adfb390439ca87898b5b805ae5a",
+    res
+  )
+);
+
+function fetchAndSend(api, res) {
+  fetch(api)
+    .then(res => {
+      return res.json();
     })
-)
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+    .then(json => {
+      res.send(json);
+    });
+}
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
