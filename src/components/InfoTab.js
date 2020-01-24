c630ae0bc19d7936a6ae17f78bf91ffb16cbcb50
strazan <sigge@labor.cool>
2020-01-24 14:12:22 +0100

    log line
    changed top: for the log line

--- src/components/InfoTab.js
+++ src/components/InfoTab.js
@@ -7,10 +7,10 @@ export default function InfoTab() {
     <div
       style={{ width: '22rem', height: '100vh', backgroundColor: '#f2f2f2' }}
     >
-      <Log />
       <p className="text-header text-color-primary">Header</p>
       <p className="text-normal text-color-primary">Normal</p>
       <p className="text-small text-color-gray">This is a paragraph</p>
+      <Log />
     </div>
   )
 }
