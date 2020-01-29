ba98e60d2f7f490926683fa4b76d3a320959276c
LPlox <lina.js.wang@gmail.com>
2020-01-29 16:39:38 +0100

    InfoTab is finished

--- src/components/InfoTab.js
+++ src/components/InfoTab.js
@@ -5,34 +5,54 @@ import './style/InfoTab.scss'
 
 export default function InfoTab({ activeDelay }) {
   console.log(activeDelay)
+  let delayColor
+  if (activeDelay) {
+    switch (activeDelay.predicted_delay_accuracy) {
+      case '100%':
+        delayColor = 'hundred-color text-normal'
+        break
+      case '80%':
+        delayColor = 'eighty-color text-normal'
+        break
+      case '60%':
+        delayColor = 'sixty-color text-normal'
+        break
+      default:
+        delayColor = 'text-color-primary text-normal'
+    }
+  }
   return (
     <div
-      className={activeDelay ? 'active' : 'hidden'}
+      className={
+        activeDelay
+          ? 'active infoDiv-shadow infoTab'
+          : 'hidden infoDiv-shadow infoTab'
+      }
       style={{ width: '22rem', height: '100vh', backgroundColor: '#f2f2f2' }}
     >
       <h1 className="text-color-primary text-header text-align-center top-margin-40">
         {activeDelay ? activeDelay.station : 'station'}
       </h1>
       <hr></hr>
-      <div className="infoDiv">
-        <div className="flex-row">
-          <p className="text-normal text-color-gray">
+      <div className="infoDiv infoDiv-shadow ">
+        <div className="flex-row ">
+          <p className="text-normal text-color-gray vertical-align">
             <ReactClock format={'HH : mm'} ticking={true} />
           </p>
         </div>
         <div className="flex-row">
-          <p className="text-color-gray text-normal">Scheduled arrival</p>
-          <p className="text-color-gray text-normal">Calculated delay</p>
-          <p className="text-color-gray text-normal">Accuracy</p>
+          <p className="text-color-primary text-normal">Scheduled arrival</p>
+          <p className="text-color-primary text-normal">Calculated delay</p>
+          <p className="text-color-primary text-normal">Accuracy</p>
         </div>
         <div className="flex-row">
-          <p className="text-color-gray text-normal">
+          <p className="text-color-primary text-normal">
             {activeDelay ? activeDelay.scheduled.slice(0, 5) : 'time'}
           </p>
-          <p className="text-color-gray text-normal">
+          <p className="text-color-primary text-normal">
             {activeDelay ? activeDelay.predicted_delay_minutes : 'minutes'}
           </p>
-          <p className="text-color-gray text-normal">
+          <p className={delayColor}>
             {activeDelay ? activeDelay.predicted_delay_accuracy : 'procent'}
           </p>
         </div>
