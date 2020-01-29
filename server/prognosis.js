c9da594aa3b928e7f8c1030c04809b7a960bb3dd
strazan <sigge@labor.cool>
2020-01-29 16:32:13 +0100

    no  more africa

--- server/prognosis.js
+++ server/prognosis.js
@@ -31,8 +31,12 @@ function getPosition(signature) {
   )
   let stringPos = ''
   pos && pos[0] ? (stringPos = pos[0].Geometry.WGS84) : (stringPos = '')
-  let lng = parseFloat(stringPos.slice(7, 25))
-  let lat = parseFloat(stringPos.slice(26, 44))
+  let lnglat = stringPos.slice(7, stringPos.length)
+  let lng = parseFloat(lnglat.split(' ')[0])
+  let lat = parseFloat(lnglat.split(' ')[1])
+  console.log(lng, lat)
+
+  // let lat = parseFloat(stringPos.slice(26, 44))
   return { longitude: lng, latitude: lat }
 }
 
